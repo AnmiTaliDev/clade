98b45aa88319cf8ee1dfeb0293ae67212c25b3c7
AnmiTaliDev <anmitali198@gmail.com>
2026-04-22 16:33:30 +0500

    feat: Add additional haplogroup imports and entries

--- src/data/haplogroups/index.ts
+++ src/data/haplogroups/index.ts
@@ -10,6 +10,7 @@ import E from "./E";
 import E1b1b from "./E1b1b";
 import F from "./F";
 import G from "./G";
+import H from "./H";
 import I from "./I";
 import I1 from "./I1";
 import I2 from "./I2";
@@ -18,13 +19,28 @@ import J from "./J";
 import J1 from "./J1";
 import J2 from "./J2";
 import K from "./K";
+import LT from "./LT";
+import L from "./L";
+import T from "./T";
 import NO from "./NO";
 import N from "./N";
 import N1 from "./N1";
 import N1c from "./N1c";
 import O from "./O";
 import C from "./C";
+import C1 from "./C1";
+import C1a from "./C1a";
+import C1a1 from "./C1a1";
+import C1a2 from "./C1a2";
+import C1b from "./C1b";
+import C1b1 from "./C1b1";
+import C1b2 from "./C1b2";
+import C1b2a from "./C1b2a";
+import C1b2b from "./C1b2b";
 import C2 from "./C2";
+import C2a from "./C2a";
+import C2b from "./C2b";
+import C2b1a1 from "./C2b1a1";
 import P from "./P";
 import Q from "./Q";
 import R from "./R";
@@ -38,28 +54,73 @@ import M_mt from "./M_mt";
 import N_mt from "./N_mt";
 import R_mt from "./R_mt";
 import HV from "./HV";
-import H from "./H";
 import H_mt from "./H_mt";
 import JT_mt from "./JT_mt";
+import J_mt from "./J_mt";
+import T_mt from "./T_mt";
 import U_mt from "./U_mt";
 import K_mt from "./K_mt";
 
 export const haplogroups: Haplogroup[] = [
-  A, BT, CT,
-  DE, D, E, E1b1b,
-  CF, C, C2,
-  F, G, H,
-  IJ, I, I1, I2,
-  J, J1, J2,
+  A,
+  BT,
+  CT,
+  DE,
+  D,
+  E,
+  E1b1b,
+  CF,
+  C,
+  C1,
+  C1a,
+  C1a1,
+  C1a2,
+  C1b,
+  C1b1,
+  C1b2,
+  C1b2a,
+  C1b2b,
+  C2,
+  C2a,
+  C2b,
+  C2b1a1,
+  F,
+  G,
+  H,
+  IJ,
+  I,
+  I1,
+  I2,
+  J,
+  J1,
+  J2,
   K,
-  NO, N, N1, N1c,
+  LT,
+  L,
+  T,
+  NO,
+  N,
+  N1,
+  N1c,
   O,
-  P, Q,
-  R, R1, R1a, R1b,
-  L_mt, L3, M_mt,
-  N_mt, R_mt,
-  HV, H_mt,
-  JT_mt, U_mt, K_mt,
+  P,
+  Q,
+  R,
+  R1,
+  R1a,
+  R1b,
+  L_mt,
+  L3,
+  M_mt,
+  N_mt,
+  R_mt,
+  HV,
+  H_mt,
+  JT_mt,
+  J_mt,
+  T_mt,
+  U_mt,
+  K_mt,
 ];
 
 export function getHaplogroup(id: string): Haplogroup | undefined {
@@ -76,7 +137,9 @@ export function getByType(type: Haplogroup["type"]): Haplogroup[] {
 
 export function getByRegion(region: string): Haplogroup[] {
   return haplogroups.filter((h) =>
-    h.distribution.some((d) => d.region.toLowerCase().includes(region.toLowerCase()))
+    h.distribution.some((d) =>
+      d.region.toLowerCase().includes(region.toLowerCase()),
+    ),
   );
 }
 
