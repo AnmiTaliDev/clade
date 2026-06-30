fd4e45eead88a57c27a159c45eae1d16e09b96a8
AnmiTaliDev <anmitali198@gmail.com>
2026-06-30 23:19:42 +0500

    Update D_mt.ts

--- src/data/haplogroups/D_mt.ts
+++ src/data/haplogroups/D_mt.ts
@@ -5,7 +5,7 @@ const D_mt: Haplogroup = {
   name: "D (mtDNA)",
   type: "mtDNA",
   parent: "M_mt",
-  children: ["D4_mt", "D5_mt"],
+  children: ["D4_mt", "D5_mt", "D1_mt"],
   formed: "~55,000 BCE",
   mrca: "~50,000 BCE",
   origin: {
