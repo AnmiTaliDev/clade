aae55d24f5ab7340a707a29b8ced2d8e7f028898
AnmiTaliDev <anmitali198@gmail.com>
2026-06-30 22:45:46 +0500

    ref: upd name

--- src/data/haplogroups/R1b-DF13.ts
+++ src/data/haplogroups/R1b-DF13.ts
@@ -2,7 +2,7 @@ import type { Haplogroup } from "@/types/haplogroup";
 
 const R1b_DF13: Haplogroup = {
   id: "R1b-DF13",
-  name: "R1b-DF13",
+  name: "R1b1a1b1a2a (DF13)",
   type: "Y-DNA",
   parent: "R1b-P312",
   children: ["R1b-L21"],
