3c7bf7032561bf0a49db67eb7127ebf5362916a9
AnmiTaliDev <anmitali198@gmail.com>
2026-06-30 22:44:44 +0500

    ref: upd name

--- src/data/haplogroups/R1b-M73.ts
+++ src/data/haplogroups/R1b-M73.ts
@@ -2,7 +2,7 @@ import type { Haplogroup } from "@/types/haplogroup";
 
 const R1b_M73: Haplogroup = {
   id: "R1b-M73",
-  name: "R1b-M73",
+  name: "R1b1a1a (M73)",
   type: "Y-DNA",
   parent: "R1b",
   children: [],
