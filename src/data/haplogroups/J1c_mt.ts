331c49ba87e233a6c634a173e49ead6b90dcb218
AnmiTaliDev <anmitali198@gmail.com>
2026-07-06 15:37:39 +0500

    Update J1c_mt.ts

--- src/data/haplogroups/J1c_mt.ts
+++ src/data/haplogroups/J1c_mt.ts
@@ -11,7 +11,7 @@ const J1c_mt: Haplogroup = {
   name: "J1c",
   type: "mtDNA",
   parent: "J1_mt",
-  children: ["J1c2_mt"],
+  children: ["J1c2_mt", "J1c3_mt"],
   formed: "~11,000 BCE",
   mrca: "~7,500 BCE",
   origin: {
